0f259f9f0503bfe2b9729148bb1159d28f80d2bf
jhillTechtonic <julie.hill@techtonic.com>
2021-02-23 13:37:19 -0600

    Step 10

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -12,6 +12,14 @@ public class App
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+        return number *2;
+    }
+
+    public static int add(int[] numbers) {
+        int sum = 0;
+        for (int x = 0; x < numbers.length; x++) {
+            sum += numbers[x];
+        }
+        return sum;
     }
 }
